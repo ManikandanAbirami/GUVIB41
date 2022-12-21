67e09ed9ed06f38f6f269fc6c50dc8c50982d059
Manikandan Anbalagan <manikandan.a@analyticbrains.com>
2022-12-21 09:33:46 +0530

    issue to resolved

--- client/guvi-b41/src/AddUser.js
+++ client/guvi-b41/src/AddUser.js
@@ -6,7 +6,7 @@ export class AddUser extends Component {
     super();
     this.state = {
       id: 0,
-      name: '',
+      username: '',
       phone: ''
     }
     console.log(this.state);
@@ -20,22 +20,28 @@ export class AddUser extends Component {
     console.log(this.state);
     console.log("inputChangeHandler called!!! - Add User");
   }
+
+  onFormSubmitted = (e) => {
+    e.preventDefault();
+    this.props.addUserHandler(this.state);
+  }
+
   render() {
     console.log("Render called!!! - Add User");
-    const { name, phone } = this.state;
+    const { username, phone } = this.state;
     return (
       <div>
         <Header heading="Add User"></Header>
         <div className='component-body-container'>
           <button className='custom-btn'>Back</button>
-          <form className='user-form'>
-            <label htmlFor='name' className='label-control'>Name: </label><br />
-            <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangeHandler}></input><br /><br />
+          <form className='user-form' onSubmit={this.onFormSubmitted.bind(this)}>
+            <label htmlFor='username' className='label-control'>Name: </label><br />
+            <input id="username" type="text" className="input-control" name="username" onChange={this.inputChangeHandler}></input><br /><br />
             <label htmlFor='phone' className='label-control'>Phone: </label><br />
             <input id="phone" type="text" className="input-control" name="phone" onChange={this.inputChangeHandler}></input><br /><br />
             <div className='user-info-container'>
               <span className='user-to-add-heading'>User to be added:</span><br />
-              <span className='user-info'>Name: {name}</span><br />
+              <span className='user-info'>Name: {username}</span><br />
               <span className='user-info'>Phone: {phone}</span>
             </div>
             <button type="submit" className='custom-btn add-btn'>Add</button>
--- client/guvi-b41/src/index.js
+++ client/guvi-b41/src/index.js
@@ -6,11 +6,12 @@ import AddUser from './AddUser';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './common/common.css'
+import PhoneDirectory from './PhoneDirectory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <AddUser />
+  <PhoneDirectory />
   // </React.StrictMode>
 );
 
