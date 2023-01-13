6125e6060958a8c276e9c8006ac145eb79911a5b
Manikandan Anbalagan <manikandan.a@analyticbrains.com>
2023-01-13 11:10:01 +0530

    promise task

--- server/promise-callback.js
+++ server/promise-callback.js
@@ -9,17 +9,38 @@ function getProduct(id) {
   });
 }
 
-
 function getListofMobiles(item, callback) {
-  setTimeout(() => {
-    console.log('Getting mobiles from the available list...')
-    callback(['Apple', 'Samsung', 'MI', 'Oppo', 'OnePlus'])
-  }, 2000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Getting mobiles from the available list...')
+      resolve(['Apple', 'Samsung', 'MI', 'Oppo', 'OnePlus'])
+    }, 2000);
+  })
 }
 
 function getItems(item, callback) {
-  setTimeout(() => {
-    console.log('Items list.....')
-    callback(['MAC', 'IPHONE', 'AirPods', 'IWatch', 'Tab'])
-  }, 2000);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Items list.....')
+      resolve(['MAC', 'IPHONE', 'AirPods', 'IWatch', 'Tab'])
+    }, 2000);
+  })
 }
+
+
+// getProduct(1, function (product) {
+//   console.log('Product', product);
+//   getListofMobiles(product.productName, (items) => {
+//     console.log('Mobiles', items);
+//     getItems(items[1], getItems(items));
+//   })
+// })
+
+// const prom = getProduct(1);
+// prom.then(product => console.log(product));
+
+getProduct(1)
+  .then(product => getListofMobiles(product.productName))
+  .then(items => getItems(items[1]));
+
+// console.log(getProduct(1));
